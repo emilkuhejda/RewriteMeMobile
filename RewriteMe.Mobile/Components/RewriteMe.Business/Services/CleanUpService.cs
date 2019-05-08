897942357b38deba19c093d33c3573321a07b2d2
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-05-08 19:14:11 +0200

    Fix static code analysis.

--- RewriteMe.Mobile/Components/RewriteMe.Business/Services/CleanUpService.cs
+++ RewriteMe.Mobile/Components/RewriteMe.Business/Services/CleanUpService.cs
@@ -30,7 +30,7 @@ namespace RewriteMe.Business.Services
             await _internalValueService.UpdateValueAsync(InternalValues.FileItemSynchronization, DateTime.MinValue).ConfigureAwait(false);
             await _internalValueService.UpdateValueAsync(InternalValues.TranscribeItemSynchronization, DateTime.MinValue).ConfigureAwait(false);
             await _internalValueService.UpdateValueAsync(InternalValues.UserSubscriptionSynchronization, DateTime.MinValue).ConfigureAwait(false);
-            await _internalValueService.UpdateValueAsync(InternalValues.ApplicationId, null);
+            await _internalValueService.UpdateValueAsync(InternalValues.ApplicationId, null).ConfigureAwait(false);
 
             await _fileItemRepository.ClearAsync().ConfigureAwait(false);
             await _userSessionRepository.ClearAsync().ConfigureAwait(false);
