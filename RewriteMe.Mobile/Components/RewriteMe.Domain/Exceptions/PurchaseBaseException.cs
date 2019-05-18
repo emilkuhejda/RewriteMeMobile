2e7100de039d23b538a77f769f37a3bb5de59ed7
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-05-18 22:05:03 +0200

    Fix build. (#18)
    * Add nuget to domain project.
    
    * Code refactoring.

---
+++ RewriteMe.Mobile/Components/RewriteMe.Domain/Exceptions/PurchaseBaseException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RewriteMe.Domain.Exceptions
+{
+    public abstract class PurchaseBaseException : Exception
+    {
+        protected PurchaseBaseException()
+        {
+        }
+
+        protected PurchaseBaseException(string message)
+            : base(message)
+        {
+        }
+
+        protected PurchaseBaseException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected PurchaseBaseException(string purchaseId, string productId)
+        {
+            PurchaseId = purchaseId;
+            ProductId = productId;
+        }
+
+        protected PurchaseBaseException(string purchaseId, string productId, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            PurchaseId = purchaseId;
+            ProductId = productId;
+        }
+
+        public string PurchaseId { get; }
+
+        public string ProductId { get; }
+    }
+}
