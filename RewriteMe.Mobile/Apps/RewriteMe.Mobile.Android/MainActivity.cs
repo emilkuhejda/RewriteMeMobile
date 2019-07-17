c4cbf8817d5f5e6636c8dd4414b388945ab81c52
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-07-17 07:52:50 +0200

    Update msal package.

--- RewriteMe.Mobile/Apps/RewriteMe.Mobile.Android/MainActivity.cs
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile.Android/MainActivity.cs
@@ -7,7 +7,6 @@ using Android.OS;
 using Android.Support.V4.App;
 using Android.Support.V4.Content;
 using FFImageLoading.Forms.Platform;
-using Microsoft.Identity.Client;
 using Plugin.InAppBilling;
 using RewriteMe.Business.Configuration;
 using RewriteMe.Mobile.Droid.Configuration;
@@ -49,7 +48,7 @@ namespace RewriteMe.Mobile.Droid
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
-            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+            //AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
             InAppBillingImplementation.HandleActivityResult(requestCode, resultCode, data);
         }
     }
