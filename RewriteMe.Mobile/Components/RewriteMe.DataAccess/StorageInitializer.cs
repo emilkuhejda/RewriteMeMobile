1fb44ecfb1698d0e17d08401e13d6d0a6fcf1072
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-08-01 13:39:35 +0200

    Add database version.

--- RewriteMe.Mobile/Components/RewriteMe.DataAccess/StorageInitializer.cs
+++ RewriteMe.Mobile/Components/RewriteMe.DataAccess/StorageInitializer.cs
@@ -7,6 +7,8 @@ namespace RewriteMe.DataAccess
 {
     public class StorageInitializer : IStorageInitializer
     {
+        private const int DatabaseVersionNumber = 0;
+
         private readonly IRecordedItemService _recordedItemService;
         private readonly IAppDbContextProvider _contextProvider;
 
@@ -34,6 +36,17 @@ namespace RewriteMe.DataAccess
                 typeof(UserSessionEntity),
                 typeof(UserSubscriptionEntity)
             };
+
+            var versionNumber = await _contextProvider.Context.GetVersionNumberAsync().ConfigureAwait(false);
+            if (versionNumber < DatabaseVersionNumber)
+            {
+                foreach (var table in tables)
+                {
+                    await _contextProvider.Context.DropTable(table).ConfigureAwait(false);
+                }
+
+                await _contextProvider.Context.UpdateVersionNumberAsync(DatabaseVersionNumber).ConfigureAwait(false);
+            }
 
             await _contextProvider.Context.CreateTablesAsync(tables).ConfigureAwait(false);
         }
