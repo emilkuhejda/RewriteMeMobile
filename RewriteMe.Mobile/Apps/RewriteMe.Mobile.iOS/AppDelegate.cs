1e9d3248a4cd35988bf971517d4be228523188ae
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-08-03 13:49:25 +0200

    Replace CollectionView with ListView.

--- RewriteMe.Mobile/Apps/RewriteMe.Mobile.iOS/AppDelegate.cs
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile.iOS/AppDelegate.cs
@@ -22,7 +22,6 @@ namespace RewriteMe.Mobile.iOS
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
-            Forms.SetFlags("CollectionView_Experimental");
             Forms.Init();
             CachedImageRenderer.Init();
 
