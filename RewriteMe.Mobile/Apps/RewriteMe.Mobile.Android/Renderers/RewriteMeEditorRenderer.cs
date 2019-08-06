846ba3b654cac686bc50ad01d6b8d2c35b173ba9
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-08-06 10:53:59 +0200

    Create Editor renderer for Android.

---
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile.Android/Renderers/RewriteMeEditorRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content;
+using Android.Graphics;
+using RewriteMe.Mobile.Droid.Renderers;
+using RewriteMe.Mobile.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(RewriteMeEditorRenderer))]
+namespace RewriteMe.Mobile.Droid.Renderers
+{
+    public class RewriteMeEditorRenderer : EditorRenderer
+    {
+        public RewriteMeEditorRenderer(Context context)
+            : base(context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var customColor = ColorPalette.EditorBorder;
+                Control.Background.SetColorFilter(customColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+            }
+        }
+    }
+}
