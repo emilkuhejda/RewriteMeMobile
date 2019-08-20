e3abe1c1d9ec19288e7c6b25ca921eaaf103ea12
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-08-20 18:11:54 +0200

    Add subscription names.

--- RewriteMe.Mobile/Apps/RewriteMe.Mobile/Transcription/SubscriptionProducts.cs
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile/Transcription/SubscriptionProducts.cs
@@ -6,12 +6,12 @@ namespace RewriteMe.Mobile.Transcription
     public static class SubscriptionProducts
     {
         public static SubscriptionProduct ProductOneHour { get; } = new SubscriptionProduct(
-            "product.subscription.1hour",
+            "product.subscription.basic",
             Loc.Text(TranslationKeys.OneHour),
             "resource://RewriteMe.Mobile.Resources.Images.Subscription-1.svg");
 
         public static SubscriptionProduct ProductTenHours { get; } = new SubscriptionProduct(
-            "product.subscription.10hours",
+            "product.subscription.advanced",
             Loc.Text(TranslationKeys.TenHours),
             "resource://RewriteMe.Mobile.Resources.Images.Subscription-10.svg");
 
