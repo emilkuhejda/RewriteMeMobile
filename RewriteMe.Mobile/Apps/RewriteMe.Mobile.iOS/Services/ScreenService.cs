c0bb84f08cbd139c1dd4bcf5d3c60155ab853360
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-08-26 10:15:22 +0200

    Prevent screen turn off when application is recording.

---
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile.iOS/Services/ScreenService.cs
@@ -0,0 +1,18 @@
+﻿using RewriteMe.Domain.Interfaces.Required;
+using UIKit;
+
+namespace RewriteMe.Mobile.iOS.Services
+{
+    public class ScreenService : IScreenService
+    {
+        public void DisableIdle()
+        {
+            UIApplication.SharedApplication.IdleTimerDisabled = true;
+        }
+
+        public void EnableIdle()
+        {
+            UIApplication.SharedApplication.IdleTimerDisabled = false;
+        }
+    }
+}
