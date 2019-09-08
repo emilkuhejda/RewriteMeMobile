d222326ef73e389cfdc8b7ba5e895386718b7617
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-09-08 16:50:54 +0200

    Update web API.

--- RewriteMe.Mobile/Components/RewriteMe.Domain/WebApi/IRewritemeAPI.cs
+++ RewriteMe.Mobile/Components/RewriteMe.Domain/WebApi/IRewritemeAPI.cs
@@ -169,6 +169,16 @@ namespace RewriteMe.Domain.WebApi
         /// </param>
         Task<HttpOperationResponse<object>> GetInformationMessagesWithHttpMessagesAsync(System.DateTime? updatedAfter = default(System.DateTime?), Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
 
+        /// <param name='informationMessageId'>
+        /// </param>
+        /// <param name='customHeaders'>
+        /// The headers that will be added to request.
+        /// </param>
+        /// <param name='cancellationToken'>
+        /// The cancellation token.
+        /// </param>
+        Task<HttpOperationResponse<object>> MarkMessageAsOpenedWithHttpMessagesAsync(System.Guid? informationMessageId = default(System.Guid?), Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
+
         /// <param name='customHeaders'>
         /// The headers that will be added to request.
         /// </param>
