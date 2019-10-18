4dbfd7eba913fffca1213a27dbf996af50bdfad0
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-10-18 12:48:21 +0200

    Fix iOS status bar color.

--- RewriteMe.Mobile/Apps/RewriteMe.Mobile.iOS/Utils/Colors.cs
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile.iOS/Utils/Colors.cs
@@ -4,6 +4,6 @@ namespace RewriteMe.Mobile.iOS.Utils
 {
     public static class Colors
     {
-        public static UIColor Primary => UIColor.FromRGB(26, 188, 156);
+        public static UIColor Primary => UIColor.FromRGB(242, 243, 244);
     }
 }
