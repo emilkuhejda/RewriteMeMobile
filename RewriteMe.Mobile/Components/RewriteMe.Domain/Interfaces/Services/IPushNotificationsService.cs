0280e70eaea245e6d691ef29e0747f767ac95e51
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-01 22:00:19 +0100

    Code refactoring.

---
+++ RewriteMe.Mobile/Components/RewriteMe.Domain/Interfaces/Services/IPushNotificationsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace RewriteMe.Domain.Interfaces.Services
+{
+    public interface IPushNotificationsService
+    {
+        Task<bool> IsEnabledAsync();
+
+        Task SetEnabledAsync(bool enabled);
+    }
+}
