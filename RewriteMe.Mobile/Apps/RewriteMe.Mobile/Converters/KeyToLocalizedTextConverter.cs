394782e953c986419f10d6fc92902c658f6f9a20
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-12-08 13:55:32 +0100

    Add slovak localization to application. (#116)
    * Add Slovak translations.
    
    * Update translation.
    
    * Fix localization of language list.
    
    * Picker localization.

---
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile/Converters/KeyToLocalizedTextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using RewriteMe.Resources.Localization;
+using Xamarin.Forms;
+
+namespace RewriteMe.Mobile.Converters
+{
+    public class KeyToLocalizedTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var key = value as string;
+            if (string.IsNullOrWhiteSpace(key))
+                return string.Empty;
+
+            return Loc.Text(key);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RewriteMe.Mobile/Components/RewriteMe.Resources/Localization/Languages.cs
+++ RewriteMe.Mobile/Components/RewriteMe.Resources/Localization/Languages.cs
@@ -4,9 +4,9 @@ namespace RewriteMe.Resources.Localization
 {
     public static class Languages
     {
-        public static LanguageInfo English { get; } = new LanguageInfo(Loc.Text(TranslationKeys.English), "en");
+        public static LanguageInfo English { get; } = new LanguageInfo(TranslationKeys.English, "en");
 
-        public static LanguageInfo Slovak { get; } = new LanguageInfo(Loc.Text(TranslationKeys.Slovak), "sk");
+        public static LanguageInfo Slovak { get; } = new LanguageInfo(TranslationKeys.Slovak, "sk");
 
         public static LanguageInfo[] All => new[] { English, Slovak };
     }
