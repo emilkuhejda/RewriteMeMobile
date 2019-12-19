c447f42df194b8bc23cbe441b40a309dbbcdcf39
emilkuhejda <emil.kuhejda@gmail.com>
2019-12-19 14:54:57 +0100

    Update request when file is uploading.

--- RewriteMe.Mobile/Components/RewriteMe.Domain/WebApi/RewriteMeClient.partial.cs
+++ RewriteMe.Mobile/Components/RewriteMe.Domain/WebApi/RewriteMeClient.partial.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 
 namespace RewriteMe.Domain.WebApi
 {
@@ -25,6 +26,22 @@ namespace RewriteMe.Domain.WebApi
                     }
                     request.Headers.TryAddWithoutValidation(header.Key, header.Value);
                 }
+            }
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, StringBuilder urlBuilder)
+        {
+            if (request.Content.Headers.ContentType.MediaType == "multipart/form-data")
+            {
+                var boundary = System.Guid.NewGuid().ToString();
+                var content = new MultipartFormDataContent(boundary);
+                content.Headers.Remove("Content-Type");
+                content.Headers.TryAddWithoutValidation("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+                var contentFile = request.Content;
+                content.Add(contentFile, "file", "file");
+
+                request.Content = content;
             }
         }
 #pragma warning restore CA1801
