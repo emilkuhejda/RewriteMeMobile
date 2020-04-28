a955264182d46ecdfbd25de28bd6c35b5282187e
emilkuhejda <emil.kuhejda@gmail.com>
2020-04-28 14:15:43 +0200

    Refactoring Record detail page.

--- RewriteMe.Mobile/Apps/RewriteMe.Mobile/GlobalSuppressions.cs
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile/GlobalSuppressions.cs
@@ -13,7 +13,6 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Reliability", "CA2008:Do not create tasks without passing a TaskScheduler", Justification = "By design", Scope = "member", Target = "~M:RewriteMe.Mobile.Commands.AsyncCommand.ExecuteAsync~System.Threading.Tasks.Task")]
 [assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "Array is OK", Scope = "member", Target = "~P:RewriteMe.Mobile.Transcription.SubscriptionProducts.All")]
 [assembly: SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "By design", Scope = "member", Target = "~M:RewriteMe.Mobile.ViewModels.ViewModelBase.Dispose(System.Boolean)")]
-[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Mobile.ViewModels.DetailBaseViewModel`1.DetailItems")]
 [assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Mobile.Transcription.PickedFile.Source")]
 [assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Mobile.Navigation.Parameters.ImportedFileNavigationParameters.Source")]
 [assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "By design", Scope = "member", Target = "~M:RewriteMe.Mobile.App.CreateContainerExtension~Prism.Ioc.IContainerExtension")]
