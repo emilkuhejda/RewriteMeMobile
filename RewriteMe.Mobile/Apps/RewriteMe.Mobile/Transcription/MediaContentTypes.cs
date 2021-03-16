4b614aa3e1c0f12af5863437dadbcba0bdbbf556
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-16 15:39:04 +0100

    Update checking unsupported types.

--- RewriteMe.Mobile/Apps/RewriteMe.Mobile/Transcription/MediaContentTypes.cs
+++ RewriteMe.Mobile/Apps/RewriteMe.Mobile/Transcription/MediaContentTypes.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace RewriteMe.Mobile.Transcription
 {
     public static class MediaContentTypes
     {
-        private static readonly HashSet<string> Mp3ContentTypes = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-        {
-            "video/mpeg",
-            "video/x-mpeg"
-        };
-
         public static bool IsUnsupported(string contentType)
         {
-            return !contentType.Contains("audio", StringComparison.OrdinalIgnoreCase) || Mp3ContentTypes.Contains(contentType);
+            return !contentType.Contains("audio", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
